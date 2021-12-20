e34818b83b5637c6ea2afeb9c2a616f0549c1d67
Richard Mlambuzi <bed-com-29-19@unima.ac.mw>
2021-12-20 10:20:04 +0200

    bus class working

--- Inheritance/Bus.java
+++ Inheritance/Bus.java
@@ -2,5 +2,78 @@ class Bus extends Vehicle{
     /**
      * initializing attributes
      */
+   private double height;
+   private double length;
+   private double width;
+   private int minimumCarryingCapacityPerSeat;
+    
+    /**
+     * constructor
+     */
+    Bus(){
 
+    }
+    
+    /**
+     * method for setting length of the bus
+     */
+
+     public void setHeight(double height){
+         this.height=height;
+     }
+
+      /**
+     * method for setting length of the bus
+     */
+
+    public void setLength(double length){
+        this.length=length;
+    }
+
+     /**
+     * method for setting length of the bus
+     */
+
+    public void setWidth (double width){
+        this.width=width;
+    }
+
+     /**
+     * method for setting length of the bus
+     */
+
+    public void setMinimumCarryingCapacityPerSeat(double minimumCarryingCapacityPerSeat){
+        this.minimumCarryingCapacityPerSeat=(int) minimumCarryingCapacityPerSeat;
+    }
+/**
+ * method for getting height of the bus
+ */
+
+ public double getHeight() {
+     return height;
+ }
+
+ /**
+ * method for getting length of the bus
+ */
+
+public double getLength() {
+    return length;
+}
+
+/**
+ * method for getting width of the bus
+ */
+
+public double getWidth() {
+    return width;
+}
+
+/**
+ * method for getting minimumCarryingCapacityPerSeat of the bus
+ */
+
+public int getMinimumCarryingCapacityPerSeat() {
+    return minimumCarryingCapacityPerSeat;
+}
 }
--- Inheritance/CarsMain.java
+++ Inheritance/CarsMain.java
@@ -4,5 +4,16 @@ public class CarsMain{
         System.out.println(hilux.getNumberOfTryes());
         hilux.setColor("red");
         System.out.println(hilux.getColor());
+
+        //creating a bus object
+        Bus schoolBus = new Bus();
+        System.out.println("This is a School Bus and has "+schoolBus.getNumberOfTryes()" tyers");
+        schoolBus.setColor("silver");
+        schoolBus.setHeight(3.81);
+        schoolBus.setLength(12);
+        schoolBus.setWidth(2.55);
+        schoolBus.setMinimumCarryingCapacityPerSeat(49);
+        System.out.println(schoolBus.getColor());
+
     }
 }
